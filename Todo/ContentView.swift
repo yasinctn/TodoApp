a1979981737e9a5aaab1bb8806ae5ff9aada47d1
Yasin Cetin <yasincetin2014@gmail.com>
2024-04-09 02:31:19 +0300

    content view functionalities completed

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -9,77 +9,126 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+  // MARK: - PROPERTIES
+  
+  @Environment(\.managedObjectContext) var managedObjectContext
+    
+  @FetchRequest(entity: Task.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Task.name, ascending: true)]) var tasks: FetchedResults<Task>
+  
+  @State private var isShowingAddTodoView: Bool = false
+  @State private var animatingButton: Bool = false
+  
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    NavigationView {
+      ZStack {
+        List {
+          ForEach(self.tasks, id: \.self) { task in
+            HStack {
+              Circle()
+                .frame(width: 12, height: 12, alignment: .center)
+                .foregroundColor(self.colorize(priority: task.priority ?? "Normal"))
+              Text(task.name ?? "Unknown")
+                .fontWeight(.semibold)
+              
+              Spacer()
+              
+              Text(task.priority ?? "Unkown")
+                .font(.footnote)
+                .foregroundColor(Color(UIColor.systemGray2))
+                .padding(3)
+                .frame(minWidth: 62)
+                .overlay(
+                  Capsule().stroke(Color(UIColor.systemGray2), lineWidth: 0.75)
+              )
+            } //: HSTACK
+              .padding(.vertical, 10)
+          } //: FOREACH
+          .onDelete(perform: deleteTask)
+        } //: LIST
+          .navigationBarTitle("Tasks", displayMode: .inline)
+          .navigationBarItems(
+            leading: EditButton().accentColor(Color.accentColor)
+        )
+        
+        // MARK: - NO TODO ITEMS
+        if tasks.count == 0 {
+          EmptyListView()
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+      } //: ZSTACK
+        .sheet(isPresented: $isShowingAddTodoView) {
+          AddTaskView().environment(\.managedObjectContext, self.managedObjectContext)
         }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        .overlay(
+          ZStack {
+            Group {
+              Circle()
+                    .fill(Color.blue)
+                .opacity(self.animatingButton ? 0.2 : 0)
+                .scaleEffect(self.animatingButton ? 1 : 0)
+                .frame(width: 68, height: 68, alignment: .center)
+              Circle()
+                    .fill(Color.blue)
+                .opacity(self.animatingButton ? 0.15 : 0)
+                .scaleEffect(self.animatingButton ? 1 : 0)
+                .frame(width: 88, height: 88, alignment: .center)
             }
-        }
+            .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true))
+            
+            Button(action: {
+              self.isShowingAddTodoView.toggle()
+            }) {
+              Image(systemName: "plus.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .background(Circle().fill(Color.white))
+                .frame(width: 48, height: 48, alignment: .center)
+            } //: BUTTON
+            .accentColor(Color.blue)
+              .onAppear(perform: {
+                 self.animatingButton.toggle()
+              })
+          } //: ZSTACK
+            .padding(.bottom, 15)
+            .padding(.trailing, 15)
+            , alignment: .bottomTrailing
+        )
+    } //: NAVIGATION
+      .navigationViewStyle(StackNavigationViewStyle())
+  }
+  
+  // MARK: - FUNCTIONS
+  
+  private func deleteTask(at offsets: IndexSet) {
+    for index in offsets {
+      let task = tasks[index]
+      managedObjectContext.delete(task)
+      
+      do {
+        try managedObjectContext.save()
+      } catch {
+        print(error)
+      }
     }
+  }
+  
+  private func colorize(priority: String) -> Color {
+    switch priority {
+    case "High":
+      return .pink
+    case "Normal":
+      return .green
+    case "Low":
+      return .blue
+    default:
+      return .gray
+    }
+  }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+// MARK: - PREVIEW
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
